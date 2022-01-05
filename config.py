43b45c3e9bd7c0649b5eee4427e205da1421f37f
montanajon <85256310+montanajon@users.noreply.github.com>
2022-01-05 21:28:49 +0100

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ class Config:
 
     #Optional Configuration
     SHUFFLE=is_enabled(os.environ.get("SHUFFLE", 'True'))
-    ADMIN_ONLY=is_enabled(os.environ.get("ADMIN_ONLY", "False"))
+    ADMIN_ONLY=is_enabled(os.environ.get("ADMIN_ONLY", "True"))
     REPLY_MESSAGE=os.environ.get("REPLY_MESSAGE", False)
     EDIT_TITLE = os.environ.get("EDIT_TITLE", True)
     #others
